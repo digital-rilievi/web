2b4e62cfcbcb30551cbf873a9cf2b0b0297c0ff5
StePiasentin <piasentin.st@gmail.com>
2024-01-18 22:36:33 +0100

    content file set globally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
-import AppBar from "./components/Navbar/Navbar";
-import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
-import PartnersList from "./components/PartnersList/PartnersList";
+import AppBar from "./components/Navbar/Navbar"
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer"
+import PartnersList from "./components/PartnersList/PartnersList"
 import styles from "./page.module.css"
-//import Head from "next/head";
+import content from 'content.json'
 
 export default function Home() {
-  var title = "Rilievi digitali, con le\ntecniche più recenti e\nall'avanguardia"
   return (
     <main>
       <AppBar />
       <VideoPlayer />
       <h1 className={styles.title}>
-        {title}
+        {content.home.title}
       </h1>
       <PartnersList
         className={styles.partnersList}

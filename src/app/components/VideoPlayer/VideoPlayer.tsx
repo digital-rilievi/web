69ef97b62f511ecf399ceca8056d65ee30753e02
Alessandro Facchin <alessandro.f@wildbrains.ai>
2024-01-18 21:37:27 +0000

    Adding poster

--- src/app/components/VideoPlayer/VideoPlayer.tsx
+++ src/app/components/VideoPlayer/VideoPlayer.tsx
@@ -10,8 +10,9 @@ const VideoPlayer = () => {
         loop
         muted
         style={{ width: '100%', height: 'auto' }}
+        poster="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro.jpeg?alt=media&token=8d16927e-b450-478f-b1f1-17ac9fb4973a"
       >
-        <source src="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro.mp4?alt=media&token=f890852e-f274-4cc8-b7a1-02d1b836a556" type="video/mp4" />
+        <source src="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro.mp4?alt=media&token=81ec90f8-2b53-403b-b445-8c9901f41f75" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>

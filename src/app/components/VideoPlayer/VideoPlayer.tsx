1dddebaf4446f8e90608f32b87360d00e175ad0e
Alessandro Facchin <alessandro.f@wildbrains.ai>
2024-01-18 20:54:56 +0000

    Unmuting it

--- src/app/components/VideoPlayer/VideoPlayer.tsx
+++ src/app/components/VideoPlayer/VideoPlayer.tsx
@@ -7,7 +7,6 @@ const VideoPlayer = () => {
       <video
         autoPlay
         loop
-        muted
         style={{ width: '100%', height: 'auto' }}
       >
         <source src="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro.mp4?alt=media&token=f890852e-f274-4cc8-b7a1-02d1b836a556" type="video/mp4" />

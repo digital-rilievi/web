13901f94a73e13f38b18ad0d69d15aa7b3e61dfe
StePiasentin <piasentin.st@gmail.com>
2024-01-18 22:19:55 +0100

    video player working properly now

--- src/app/components/VideoPlayer/VideoPlayer.tsx
+++ src/app/components/VideoPlayer/VideoPlayer.tsx
@@ -1,12 +1,28 @@
 
 'use client'
+import { useEffect } from "react";
 
 const VideoPlayer = () => {
+
+  useEffect(() => {
+    const startPlayback = () => {
+      const video = document.getElementById('myVideo') as HTMLVideoElement | null;
+      if (video) {
+        video.play();
+      }
+    };
+
+    startPlayback();
+  }, []);
+
   return (
     <div>
       <video
+        id="myVideo"
         autoPlay
         loop
+        muted
+        playsInline
         style={{ width: '100%', height: 'auto' }}
       >
         <source src="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro.mp4?alt=media&token=f890852e-f274-4cc8-b7a1-02d1b836a556" type="video/mp4" />

281e062b8385da29490ed1d64112741c04acba6d
Alessandro Facchin <alessandro.f@wildbrains.ai>
2024-02-07 11:49:45 +0000

    Fixing video links

--- src/app/components/VideoPlayer/VideoPlayer.tsx
+++ src/app/components/VideoPlayer/VideoPlayer.tsx
@@ -24,9 +24,9 @@ const VideoPlayer = () => {
       style={{ width: '100%', height: 'auto' }}
       poster="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro.jpeg?alt=media&token=8d16927e-b450-478f-b1f1-17ac9fb4973a"
     >
-      <source src="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro.mp4?alt=media&token=81ec90f8-2b53-403b-b445-8c9901f41f75" type="video/mp4" />
-      {/* High res video: https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro_high.mp4?alt=media&token=e3cd38b5-9503-4c87-9538-20b5a0ead246 */}
-      {/* Low res video: https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro.mp4?alt=media&token=81ec90f8-2b53-403b-b445-8c9901f41f75 */}
+      <source src="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro_high.mp4?alt=media&token=3c905773-39c6-491c-85d1-8eed21f813a2" type="video/mp4" />
+      {/* High res video: https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro_high.mp4?alt=media&token=3c905773-39c6-491c-85d1-8eed21f813a2 */}
+      {/* Low res video: https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro.mp4?alt=media&token=7182c3d6-3487-4af8-8978-99ac3ab9c952 */}
       Your browser does not support the video tag.
     </video>
   )

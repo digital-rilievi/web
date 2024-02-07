1521d7d2d68f17c0ce6ce3e59586a01d78584281
Alessandro Facchin <alessandro.f@wildbrains.ai>
2024-02-07 14:27:10 +0000

    Fixing values and titles

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -37,7 +37,7 @@ const Navbar = (props: NavBarProps) => {
             I NOSTRI PROGETTI
           </Link>
           <Link href="/testimonials" className={pathname === '/testimonials' ? styles.selected : styles.unselected}>
-            CI RACCONTANO COSI
+            CI RACCONTANO COSÌ
           </Link>
         </div>
 

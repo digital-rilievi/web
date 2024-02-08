04e00e13f2b54b8d61456406e5d5ebeb989e433f
StePiasentin <piasentin.st@gmail.com>
2024-02-08 02:00:58 +0100

    tooltip now follows the cursor

--- src/app/components/WhoWeAre/WhoWeAre.tsx
+++ src/app/components/WhoWeAre/WhoWeAre.tsx
@@ -1,69 +1,69 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Space from '../Space/Space'
 import styles from './WhoWeAre.module.css'
-import { Tooltip } from 'react-tooltip'
 import CustomTooltip from '../CustomTooltip/CustomTooltip'
 import content from 'content.json'
 
 interface Item {
     name: string,
     role: string,
-    title1: string,
-    desc1: string,
-    title2: string,
-    desc2: string,
     tooltipQuote: string,
     tooltipImage: string
 }
 
+
+
 const WhoWeAre = () => {
-    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 })
-
+    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+    const [gridContainerOffset, setGridContainerOffset] = useState({ top: 0, left: 0 });
+    const [hoveredItem, setHoveredItem] = useState<Item | null>(null);
+  
+    const handleMouseEnter = (item: Item) => {
+      setHoveredItem(item);
+    };
+  
+    const handleMouseLeave = () => {
+      setHoveredItem(null);
+    };
+  
     const handleMouseMove = (e: React.MouseEvent) => {
-        setCursorPosition({ x: e.clientX, y: e.clientY })
-    }
-
-    const itemsPerRow = 3
-    const totalItems = content['chi-siamo'].whoWeAreList.length
-    const rest = totalItems % itemsPerRow
-    var remainingItems
-    if (rest > 0)
-        remainingItems = itemsPerRow - (totalItems % itemsPerRow)
-    else
-        remainingItems = 0
-
-    // Add invisible placeholder items to fill the last row
-    const filledItems = remainingItems > 0 ? [...content['chi-siamo'].whoWeAreList, ...Array(remainingItems).fill(null)] : content['chi-siamo'].whoWeAreList
-
+      setCursorPosition({ x: e.clientX, y: e.clientY });
+      const gridContainer = document.querySelector(`.${styles.gridContainer}`);
+      if (gridContainer) {
+        const rect = gridContainer.getBoundingClientRect();
+        setGridContainerOffset({ top: rect.top, left: rect.left });
+      }
+    };
+  
     return (
-        <div>
-            <p className={styles.whoWeAreTitle}>CHI SIAMO</p>
-            <Space />
-            <div className={styles.gridContainer}>
-                {filledItems.map((item, index) => (
-                    <div className={styles.item} key={index} onMouseMove={handleMouseMove}>
-                        {item && ( // Render only if item exists (skip invisible placeholders)
-                            <div id={`my-anchor-element${index}`} className={styles.nameColumn}>
-                                <div className={styles.title}>{item.name}</div>
-                                <div className={styles.desc}>{item.role}</div>
-                                {(item.tooltipQuote || item.tooltipImage) && (
-                                    <Tooltip
-                                        arrowColor={'transparent'}
-                                        className={styles.tooltipContainer}
-                                        anchorSelect={`#my-anchor-element${index}`}
-                                        style={{ top: cursorPosition.y, left: cursorPosition.x }}
-                                    >
-                                        <CustomTooltip quote={item.tooltipQuote} img={item.tooltipImage} />
-                                    </Tooltip>
-                                )}
-                            </div>
-                        )}
-                    </div>
-                ))}
+      <>
+        <p className={styles.whoWeAreTitle}>CHI SIAMO</p>
+        <Space />
+        <div className={styles.gridContainer} onMouseMove={handleMouseMove}>
+          {content['chi-siamo'].whoWeAreList.map((item, index) => (
+            <div
+              className={styles.item}
+              key={index}
+              onMouseEnter={() => handleMouseEnter(item)}
+              onMouseLeave={handleMouseLeave}
+            >
+              {item && (
+                <div id={`my-anchor-element${index}`} className={styles.nameColumn}>
+                  <div className={styles.title}>{item.name}</div>
+                  <div className={styles.desc}>{item.role}</div>
+                </div>
+              )}
+              {hoveredItem === item && (
+                <div className={styles.tooltipContainer} style={{ top: cursorPosition.y - gridContainerOffset.top, left: cursorPosition.x - gridContainerOffset.left, position: 'absolute' }}>
+                  <CustomTooltip quote={item.tooltipQuote} img={item.tooltipImage} />
+                </div>
+              )}
             </div>
+          ))}
         </div>
-    )
-}
-
-export default WhoWeAre
+      </>
+    );
+  };
+  
+  export default WhoWeAre;

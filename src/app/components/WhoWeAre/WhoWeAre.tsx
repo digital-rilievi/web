81bc337e854e152aaf2c8bc40db20fff0e6f142c
StePiasentin <piasentin.st@gmail.com>
2024-02-10 01:18:20 +0100

    fixed tooltip bug in flipping when page zoomed out

--- src/app/components/WhoWeAre/WhoWeAre.tsx
+++ src/app/components/WhoWeAre/WhoWeAre.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Space from '../Space/Space'
 import styles from './WhoWeAre.module.css'
 import CustomTooltip from '../CustomTooltip/CustomTooltip'
@@ -14,24 +14,24 @@ interface Item {
 
 
 const WhoWeAre = () => {
-    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
-    const [gridContainerOffset, setGridContainerOffset] = useState({ top: 0, left: 0 });
-    const [hoveredItem, setHoveredItem] = useState<Item | null>(null);
+    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 })
+    const [gridContainerOffset, setGridContainerOffset] = useState({ top: 0, left: 0 })
+    const [hoveredItem, setHoveredItem] = useState<Item | null>(null)
 
     const handleMouseEnter = (item: Item) => {
-        setHoveredItem(item);
+        setHoveredItem(item)
     };
 
     const handleMouseLeave = () => {
-        setHoveredItem(null);
+        setHoveredItem(null)
     };
 
     const handleMouseMove = (e: React.MouseEvent) => {
         setCursorPosition({ x: e.clientX, y: e.clientY });
-        const gridContainer = document.querySelector(`.${styles.gridContainer}`);
+        const gridContainer = document.querySelector(`.${styles.gridContainer}`)
         if (gridContainer) {
-            const rect = gridContainer.getBoundingClientRect();
-            setGridContainerOffset({ top: rect.top, left: rect.left });
+            const rect = gridContainer.getBoundingClientRect()
+            setGridContainerOffset({ top: rect.top, left: rect.left })
         }
     };
 
@@ -39,52 +39,46 @@ const WhoWeAre = () => {
         const { x, y } = cursorPosition;
         const { top, left } = gridContainerOffset;
         const tooltipElement = document.querySelector(`.${styles.tooltipContainer}`) as HTMLElement;
-      
+    
         if (!tooltipElement) {
-          return { top: 0, left: 0 };
+            return { top: -100000, left: -100000 };
         }
-      
-        // Get actual dimensions and position of the tooltip dynamically
+    
         const tooltipRect = tooltipElement.getBoundingClientRect();
-      
+    
         // Calculate initial positions
         let tooltipTop = y - top;
         let tooltipLeft = x - left;
-      
+    
         // Adjust positions to keep the tooltip within the screen boundaries
-        if (tooltipTop + tooltipRect.height > window.innerHeight) {
-          tooltipTop = window.innerHeight - tooltipRect.height;
+        if (tooltipTop + tooltipRect.height > document.body.clientHeight) {
+            tooltipTop = document.body.clientHeight - tooltipRect.height;
         } else if (tooltipTop < 0) {
-          tooltipTop = 0;
+            tooltipTop = 0;
         }
-      
-        if (tooltipLeft + tooltipRect.width > window.innerWidth) {
-          tooltipLeft = window.innerWidth - tooltipRect.width;
+    
+        if (tooltipLeft + tooltipRect.width > document.body.clientWidth) {
+            tooltipLeft = document.body.clientWidth - tooltipRect.width;
         } else if (tooltipLeft < 0) {
-          tooltipLeft = 0;
+            tooltipLeft = 0;
         }
-      
-        // If tooltip is close to the right edge, mirror to the opposite side
-        if (x - left > window.innerWidth - (x - tooltipLeft) - tooltipRect.width) {
-          tooltipLeft = x - left - tooltipRect.width;
+    
+        // Mirror to the opposite side if tooltip is close to the right edge
+        if (document.body.clientWidth - (x - left) < tooltipRect.width) {
+            tooltipLeft = x - left - tooltipRect.width;
         }
-      
-        // If tooltip is close to the bottom edge, mirror to the opposite side
-        if (y - top > window.innerHeight - (y - tooltipTop) - tooltipRect.height) {
-          tooltipTop = y - top - tooltipRect.height;
+    
+        // Mirror to the opposite side if tooltip is close to the bottom edge
+        if (document.body.clientHeight - (y - top) < tooltipRect.height) {
+            tooltipTop = y - top - tooltipRect.height;
         }
+    
+        return { top: tooltipTop, left: tooltipLeft };
+    };
+    
+    
+    
       
-        // Build the final object
-        const tooltipPosition: React.CSSProperties = {
-          top: tooltipTop,
-          left: tooltipLeft,
-        };
-      
-        return tooltipPosition;
-      };
-      
-      
-
     return (
         <>
             <p className={styles.whoWeAreTitle}>CHI SIAMO</p>
@@ -112,7 +106,7 @@ const WhoWeAre = () => {
                 ))}
             </div>
         </>
-    );
-};
+    )
+}
 
-export default WhoWeAre;
+export default WhoWeAre

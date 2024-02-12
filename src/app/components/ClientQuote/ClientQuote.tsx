b874b18ab8a8ec130ef6fbb108c69c02a6647158
StePiasentin <piasentin.st@gmail.com>
2024-02-12 03:16:28 +0100

    fixed client quote style

--- src/app/components/ClientQuote/ClientQuote.tsx
+++ src/app/components/ClientQuote/ClientQuote.tsx
@@ -15,24 +15,36 @@ interface ClientQuoteProps {
 
 const ClientQuote = (props: ClientQuoteProps) => {
     return (
-        <div className={styles.rowContainer}>
-            <div className={styles.column}>
-                <Image src={props.imageSrc} alt={''} height={1} width={1} className={styles.image} unoptimized={true} />
-                <h3 className={styles.name}>{props.name}</h3>
-                <br />
-                <p>{props.description}</p>
-            </div>
+        <div className={styles.divContainer}>
 
-            <div className={styles.row}>
-                <Image src={'/assets/extras/quotes.svg'} width={1} height={1} alt={''} className={styles.icon} unoptimized={true} />
-                <div>
-                <Image src={'/assets/extras/quotes.svg'} width={1} height={1} alt={''} className={styles.quoteInvisibleIcon} unoptimized={true} />
-                <span>{props.quote}</span>
+            <div className={styles.rowContainer}>
+                <div className={styles.column}>
+                    <Image src={props.imageSrc} alt={''} height={1} width={1} className={styles.image} unoptimized={true} />
+                </div>
+
+                <div className={styles.row}>
+                    <Image src={'/assets/extras/quotes.svg'} width={1} height={1} alt={''} className={styles.icon} unoptimized={true} />
+                    <div>
+                        <Image src={'/assets/extras/quotes.svg'} width={1} height={1} alt={''} className={styles.quoteInvisibleIcon} unoptimized={true} />
+                        <span className={styles.quote}>{props.quote}</span>
+                    </div>
+                </div>
+
+                <div className={styles.button}>
+                    <CircularButton link={props.link} label={props.linkLabel} remainsInPage />
                 </div>
             </div>
+            <div className={styles.desc}>
+                <div className={styles.column}>
 
-            <div className={styles.button}>
-                <CircularButton link={props.link} label={props.linkLabel} remainsInPage />
+                    <h3 className={styles.name}>{props.name}</h3>
+                    <br />
+                    <p>{props.description}</p>
+                </div>
+                <div style={{ "flex": 2 }} />
+                <div className={styles.invisibleButton}>
+                    <CircularButton link={props.link} label={props.linkLabel} remainsInPage />
+                </div>
             </div>
         </div>
     );

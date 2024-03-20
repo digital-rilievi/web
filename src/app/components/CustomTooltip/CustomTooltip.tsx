7aa342d3088e6561710adc6f4f76c2b6523f57db
StePiasentin <piasentin.st@gmail.com>
2024-03-20 22:26:17 +0100

    fixed more who we are layout for mobile

--- src/app/components/CustomTooltip/CustomTooltip.tsx
+++ src/app/components/CustomTooltip/CustomTooltip.tsx
@@ -12,20 +12,20 @@ const CustomTooltip = (props: TooltipProps) => {
     return (
         <div className={styles.tooltipContainer}>
             {props.img && (
-                <ImageWrapper className={styles.tooltipImage} alt={''} src={props.img} />
+                <ImageWrapper className={styles.tooltipImage} imagestyle={styles.image} alt={''} src={props.img} />
             )}
             {props.quote && (
                 <div className={styles.quoteWrapper}>
                     <Image className={styles.tooltipQuote} src={'/assets/extras/quotes.svg'} alt={''} height={1} width={1} />
-                    
                     <div>
-                    <Image className={styles.tooltipQuote} src={'/assets/extras/quotes.svg'} style={{visibility: "hidden"}} alt={''} height={1} width={1} />
+                    <Image className={`${styles.tooltipQuote} ${styles.tooltipQuoteInvisible}`} src={'/assets/extras/quotes.svg'} alt={''} height={1} width={1} />
                     <p className={styles.tooltipText}>{props.quote}</p>
                 </div>
                 </div>
             )}
         </div>
     )
+
 }
 
 export default CustomTooltip

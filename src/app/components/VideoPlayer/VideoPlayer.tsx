ec1825678fa2c8b634742c332801c2139a75d829
Alessandro Facchin <alessandro.f@wildbrains.ai>
2024-07-29 15:11:12 +0200

    Updating content and token

--- src/app/components/VideoPlayer/VideoPlayer.tsx
+++ src/app/components/VideoPlayer/VideoPlayer.tsx
@@ -37,7 +37,7 @@ const VideoPlayer = (props: VideoPlayerProps) => {
         poster="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro.jpeg?alt=media&token=5323f5ec-28a5-405e-a387-a17b74e613d1"
       >
         <source src="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro_noaudio.webm?alt=media&token=cbd3d82c-cf49-4a03-bccf-525d0bbe375a" type="video/webm" />
-        <source src="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro_noaudio.mp4?alt=media&token=a0dc6fa6-1276-4afe-be01-55e104927794" type="video/mp4" />
+        <source src="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro.mp4?alt=media&token=1ea9086c-61b9-444e-b8c0-df2f8d518062" type="video/mp4" />
       </video>
     </div>
   )

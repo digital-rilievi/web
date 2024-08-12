abfd1cb347a5d2c78445d941a3c85fb9f92968e7
StePiasentin <piasentin.st@gmail.com>
2024-08-12 17:42:13 +0200

    wip - added custom controls for test purpose

--- src/app/components/VideoPlayer/VideoPlayer.tsx
+++ src/app/components/VideoPlayer/VideoPlayer.tsx
@@ -1,9 +1,11 @@
 
 'use client'
-import { useEffect } from "react"
+import { useEffect, useRef, useState } from "react"
 import styles from './VideoPlayer.module.css'
 import useDeviceInfo from '../../hooks/useDeviceInfo'
 import ImageWrapper from "../ImageWrapper/ImageWrapper"
+import CircularButton from "../CircularButton/CircularButton"
+import { isUtf8 } from "buffer"
 
 interface VideoPlayerProps {
   title?: string,
@@ -19,19 +21,34 @@ const VideoPlayer = (props: VideoPlayerProps) => {
       const video = document.getElementById('myVideo') as HTMLVideoElement | null
       if (video) {
         video.play()
+        setIsPlaying(!isPlaying)
       }
     }
 
     startPlayback()
   }, [])
 
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const handlePlayPause = () => {
+    if (videoRef.current) {
+      if (isPlaying) {
+        videoRef.current.pause()
+      } else {
+        videoRef.current.play()
+      }
+      setIsPlaying(!isPlaying)
+    }
+  };
+
   const [major, minor = 0] = iosVersion.split('.').map(Number)
   const versionNumber = major + (minor / 10)
   var iosBiggerThan17 = versionNumber >= 17.0
 
-  if(browser) console.log("browser: " + browser)
-  if(device) console.log("device: " + device)
-  if(iosVersion) console.log("iosVersion: " + iosVersion)
+  if (browser) console.log("browser: " + browser)
+  if (device) console.log("device: " + device)
+  if (iosVersion) console.log("iosVersion: " + iosVersion)
 
   return (
     <div style={{ position: "relative" }}>
@@ -39,19 +56,30 @@ const VideoPlayer = (props: VideoPlayerProps) => {
         {props.title}
       </h1>
       {(device !== "iOS" || (device === "iOS" && browser === "Safari" && !iosBiggerThan17) ?
-        <video
-          className="intro-video"
-          id="myVideo"
-          playsInline
-          autoPlay
-          loop
-          muted
-          style={props.videoStyle}
-          poster="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro_lowres.jpeg?alt=media&token=cbf28c19-b5d6-42d1-a2f3-39fbfc9dee5f"
-        >
-          <source src="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro_noaudio.webm?alt=media&token=c0b9afee-2823-4af5-a228-80e2908aa521" type="video/webm" />
-          <source src="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro.mp4?alt=media&token=9344e094-3889-4ba4-9b2c-2115b7d431e3" type="video/mp4" />
-        </video> :
+        <div className={styles.videoAndCommandsWrapper}>
+          <video
+            className="intro-video"
+            id="myVideo"
+            ref={videoRef}
+            playsInline
+            autoPlay={(device === "iOS" && browser === "Safari") ? true : false}
+            loop
+            controls={false}
+            muted
+            style={props.videoStyle}
+            poster="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro_lowres.jpeg?alt=media&token=cbf28c19-b5d6-42d1-a2f3-39fbfc9dee5f"
+          >
+            <source src="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro_noaudio.webm?alt=media&token=c0b9afee-2823-4af5-a228-80e2908aa521" type="video/webm" />
+            <source src="https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro.mp4?alt=media&token=9344e094-3889-4ba4-9b2c-2115b7d431e3" type="video/mp4" />
+          </video>
+          {((device === "iOS" && browser === "Safari") &&
+            <div className={styles.commandsWrapper}>
+            <div className={styles.circularButton} onClick={handlePlayPause}>
+              {isPlaying ? "METTI IN PAUSA" : "RIPRODUCI"}
+            </div>
+          </div>
+          )}
+        </div> :
         <ImageWrapper
           className={styles.image}
           src={`https://firebasestorage.googleapis.com/v0/b/digital-rilievi.appspot.com/o/intro_lowres.jpeg?alt=media&token=cbf28c19-b5d6-42d1-a2f3-39fbfc9dee5f`}
